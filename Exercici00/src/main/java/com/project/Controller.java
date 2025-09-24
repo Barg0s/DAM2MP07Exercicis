b6ee5494802dd7b57ce972e8b40b7f1fd8de5f15
Barg0s <150375261+Barg0s@users.noreply.github.com>
2025-09-24 13:22:13 +0200

    calculadora

--- Exercici00/src/main/java/com/project/Controller.java
+++ Exercici00/src/main/java/com/project/Controller.java
@@ -3,65 +3,89 @@ package com.project;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+
+import javax.swing.Action;
+
 import javafx.event.ActionEvent;
+
 public class Controller {
 
     @FXML
-    private Button buttonSuma,buttonResta,buttonMulti,buttonDivi,buttonIgual;
-
+    private Button buttonSuma, buttonResta, buttonMulti, buttonDivi, buttonIgual;
 
     @FXML
     private TextField container;
 
     private String operador = "";
-    private int primer = 0;
-    private int segon = 0;
-    
+    private double primer = 0;
+    private double segon = 0;
 
+    @FXML
+    private void ObtenirNum(ActionEvent e) {
+        Button boto = (Button) e.getSource();
+        String textBoto = boto.getText();
+        container.appendText(textBoto);
+    }
 
     @FXML
     private void ObtenirBoto(ActionEvent event) {
-        Button boto = (Button) event.getSource();
-        String textBoto = boto.getText();
-
-        if (!"+-*/=".contains(textBoto)) {
-            container.appendText(textBoto);
-            primer = Integer.parseInt(container.getText());
-        } else {
-            operador = textBoto;
-            container.clear();
+        if (!container.getText().isEmpty()) {
+            primer = Double.parseDouble(container.getText());
+            Button boto = (Button) event.getSource();
+            operador = boto.getText(); 
+            container.clear(); 
         }
     }
 
-        @FXML
-private void Operar(ActionEvent event) {
-    Button boto = (Button) event.getSource(); //https://www.quora.com/How-do-I-check-if-a-button-in-JavaFX-is-pressed
-        if (!boto.getText().equals("+") && !boto.getText().equals("-") 
-            && !boto.getText().equals("*") && !boto.getText().equals("/") && !boto.getText().equals("=")) {
-            segon = Integer.parseInt(boto.getText());
-            }
-            double r = 0;
+    @FXML
+    private void Operar(ActionEvent event) {
+        if (container.getText().isEmpty() || operador.isEmpty()) return;
 
-    switch (operador) {
-        case "+": r = primer + segon; 
-        break;
-        case "-": r = primer - segon;
-         break;
-        case "*": r = primer * segon; 
-        break;
-        case "/": 
-                if (segon == 0){
-                    container.setText("no es pot dividir entre 0");
+        segon = Double.parseDouble(container.getText());
+        double r = 0;
+
+        switch (operador) {
+            case "+":
+                r = primer + segon;
+                break;
+            case "-":
+                r = primer - segon;
+                break;
+            case "*":
+                r = primer * segon;
+                break;
+            case "/":
+                if (segon == 0) {
+                    container.setText("No es pot dividir entre 0");
                     return;
-                }else{
-                r = primer / segon; 
-}
-            break;
+                } else {
+                    r = primer / segon;
+                }
+                break;
+        
+            default:
+                break;
+        }
+
+
+        container.setText(String.valueOf(r));
+        primer = r;  
+        operador = ""; 
     }
 
-    operador = ""; 
-    container.clear();
-    container.setText(String.valueOf(r));
-}
+    @FXML
+    private void netejarContainer(ActionEvent e){
+        container.clear();
+        primer = 0;
+        operador = "";
     }
+
+    @FXML
+    private void eliminarUltim(ActionEvent e){
+        if (!container.getText().isEmpty()){ //https://stackoverflow.com/questions/27950596/how-to-build-a-delete-button-to-remove-a-character-from-the-textfield-every-time
+            container.setText( container.getText().substring(0, container.getText().length()-1));
+
+        }
+
+    }
+}
