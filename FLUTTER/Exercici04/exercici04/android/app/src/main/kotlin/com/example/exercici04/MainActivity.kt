e82186b3931e913d211f0d3a4949fd6a1686dbd7
Barg0s <150375261+Barg0s@users.noreply.github.com>
2026-03-13 16:30:48 +0100

    .

--- FLUTTER/Exercici04/exercici04/android/app/src/main/kotlin/com/example/exercici04/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.exercici04
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ FLUTTER/exercici05A/exercici05a/android/app/src/main/kotlin/com/example/exercici05a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exercici05a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
